c6fc65f2acc0c3581cde993891a266eb446f8547
ashutosh-kumar <aashutosh.blaze@gmail.com>
2025-06-26 17:56:32 +0530

    Get By Id Test Case for course controller

--- src/test/kotlin/com/practice/student_course_system/courses/CourseControllerTest.kt
+++ src/test/kotlin/com/practice/student_course_system/courses/CourseControllerTest.kt
@@ -10,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.http.MediaType
 import org.springframework.test.context.bean.override.mockito.MockitoBean
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
@@ -26,10 +27,11 @@ class CourseControllerTest {
     private lateinit var courseService: CourseService
 
     val objectMapper = ObjectMapper().registerKotlinModule()
+    val id = 99L
     val courseDto = CourseDTO(title = "Spring Boot", description = "Learn Backend")
     val invalidCourseDto = CourseDTO(title = "", description = "Learn Backend")
     val course = Course(
-        id = 1L,
+        id = 99L,
         title = "Kotlin Basics",
         description = "Intro to Kotlin",
         students = mutableSetOf()
@@ -48,7 +50,7 @@ class CourseControllerTest {
         )
             .andExpect(status().isCreated)
             .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-            .andExpect(jsonPath("$.id").value(1L))
+            .andExpect(jsonPath("$.id").value(99L))
             .andExpect(jsonPath("$.title").value("Kotlin Basics"))
             .andExpect(jsonPath("$.description").value("Intro to Kotlin"))
     }
@@ -65,6 +67,23 @@ class CourseControllerTest {
             .andExpect(status().isBadRequest)
             .andExpect(content().contentType(MediaType.APPLICATION_JSON))
             .andExpect(jsonPath("$.title").value("Title cannot be blank"))
+    }
+
+    @Test
+    fun `should return course when course with given id exists`(){
+        whenever(courseService.getCourseById(id)).thenReturn(course)
+
+        val courseJson = objectMapper.writeValueAsString(id)
+
+        mockMvc.perform(
+            get("/courses/$id")
+                .contentType(MediaType.APPLICATION_JSON)
+        )
+            .andExpect(status().isOk)
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.id").value(id))
+            .andExpect(jsonPath("$.title").value("Kotlin Basics"))
+            .andExpect(jsonPath("$.description").value("Intro to Kotlin"))
     }
 
 }
